a5a49f6be1b593810b3ca704b24329eeb80f8a31
sraul <sudarshan_raul@intuit.com>
2018-02-23 10:26:47 +0530

    Testing GetData

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/getData/:id', (req, res) => getData(req, res))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+function getData(req, res) {
+  res.send("id = " + req.params.id);
+}
